01bfeab4662f7edb48077b35b1a3ae1795f8133f
Egor Ikonnikov <seelenchik4@gmail.com>
2025-07-17 15:33:17 +0200

    Now labubu is foxetgsi

--- Tools/vendoroverlay/rw-system-add.sh
+++ Tools/vendoroverlay/rw-system-add.sh
@@ -1,4 +1,4 @@
 # Vendor overlays
-mount -o bind /system/labubu/vo /vendor/overlay || true
-mount -o bind /system/labubu/group /vendor/etc/group || true
-mount -o bind /system/labubu/passwd /vendor/etc/passwd || true
+mount -o bind /system/foxetgsi/vo /vendor/overlay || true
+mount -o bind /system/foxetgsi/group /vendor/etc/group || true
+mount -o bind /system/foxetgsi/passwd /vendor/etc/passwd || true
