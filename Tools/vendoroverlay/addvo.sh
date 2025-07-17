4259503ca1c065cc5caac211aa5ca8b1f5b56bbb
Egor Ikonnikov <seelenchik4@gmail.com>
2025-07-17 15:32:50 +0200

    Now labubu is foxetgsi

--- Tools/vendoroverlay/addvo.sh
+++ Tools/vendoroverlay/addvo.sh
@@ -3,11 +3,11 @@
 SCRIPT_DIR=$(dirname "$0")
 BASE_DIR="$1"
 
-mkdir -p "$BASE_DIR/system/labubu"
-mkdir -p "$BASE_DIR/system/labubu/vo"
+mkdir -p "$BASE_DIR/system/foxetgsi"
+mkdir -p "$BASE_DIR/system/foxetgsi/vo"
 
-cp -r "$BASE_DIR/vendor/overlay/." "$BASE_DIR/system/labubu/vo/"
-cp "$BASE_DIR/vendor/etc/passwd" "$BASE_DIR/system/labubu/"
-cp "$BASE_DIR/vendor/etc/group" "$BASE_DIR/system/labubu/"
+cp -r "$BASE_DIR/vendor/overlay/." "$BASE_DIR/system/foxetgsi/vo/"
+cp "$BASE_DIR/vendor/etc/passwd" "$BASE_DIR/system/foxetgsi/"
+cp "$BASE_DIR/vendor/etc/group" "$BASE_DIR/system/foxetgsi/"
 
 echo "" >> "$BASE_DIR/system/bin/rw-system.sh" && cat "$SCRIPT_DIR/rw-system-add.sh" >> "$BASE_DIR/system/bin/rw-system.sh"
